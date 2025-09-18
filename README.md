743049db36f3b9dce30248144647c9a8aab47e15
OrangeCatMan <83187564+OrangeCatMan@users.noreply.github.com>
2025-09-18 13:15:41 +0000

    Co-authored-by: lolp00 <lolp00@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python_pong_2025
 from the 2 greatest programmers of all time... for python class 2025.... the classic game.... PONG.
+
+|\     ___
+| \   |   |
+| /   |   |
+|/    |   |
+|     |   |
+|     |___|
---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import turtle
+
+#The madness begins...
+
+
+def main():
+    turtle.setup(700,700)
+    turtle.bgcolor("black")
+    turtle.bgcolor()
+
+    
+    # add stuff here
+    bill = turtle.Turtle()
+    bill.pencolor('white')
+    bill.pencolor()
+    # turtle.ht()
+
+    #now you can do stuff \/
+
+main()
+    
