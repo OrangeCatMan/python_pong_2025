eb034c11456fe13e18f70bad3f4876e7b2fbc60d
OrangeCatMan <83187564+OrangeCatMan@users.noreply.github.com>
2025-09-19 13:15:54 +0000

    Co-authored-by: lolp00 <lolp00@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # python_pong_2025
 from the 2 greatest programmers of all time... for python class 2025.... the classic game.... PONG.
-
-|\     ___
-| \   |   |
-| /   |   |
-|/    |   |
-|     |   |
-|     |___|
+"keep the money flipping 
+its all i know
+been in and out of prison for a while i know
+grew up in the ghetto
+thuggin in the game
+they did their best to break us forever we remain
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed
+untamed"
+-rhys
+|\     ___/n
+| \   |   |/n
+| /   |   |/n
+|/    |   |/n
+|     |   |/n
+|     |___|/n
--- main.py
+++ main.py
@@ -1,22 +1,63 @@
 import tkinter as tk
 import turtle
+#USE .TELEPORT
 
-#The madness begins...
+#setup
+screen = turtle.Screen()
+turtle.setup(800,800)
+turtle.bgcolor("black")
+turtle.bgcolor()
+lPaddle = turtle.Turtle() 
+rPaddle = turtle.Turtle()
+ball = turtle.Turtle()
+midLine = turtle.Turtle()
+midLine.ht()
+rPaddle.ht()
+lPaddle.ht()
+ball.ht()
+midLine.pencolor('white')
+midLine.pencolor()
+lPaddle.pencolor('white')
+lPaddle.pencolor()
+rPaddle.pencolor('white')
+rPaddle.pencolor()
+ball.pencolor('white')
+ball.pencolor()
+midLine.left(90)
+rPaddle.left(90)
+lPaddle.left(90)
+midLine.width(10)
+screen.delay(0)
+screen.delay()
+rPaddle.fillcolor('white')
+lPaddle.fillcolor('white')
+midLine.penup()
+midLine.goto(0,-370)
 
+for i in range(20):
+    midLine.pendown()
+    midLine.forward(10)
+    midLine.penup()
+    midLine.forward(30)
+
+#left paddle setup
+
+def lPaddle_setup():
+    lPaddle.begin_fill()
+    for i in range(4):
+        lPaddle.forward(10)
+        lPaddle.left(90)
+    lPaddle.end_fill()
+
+def rPaddle_setup():
+    rPaddle.begin_fill()
+    for i in range(4):
+        rPaddle.forward(10)
+        rPaddle.left(90)
+    rPaddle.end_fill()
+    
 
 def main():
-    turtle.setup(700,700)
-    turtle.bgcolor("black")
-    turtle.bgcolor()
-
-    
-    # add stuff here
-    bill = turtle.Turtle()
-    bill.pencolor('white')
-    bill.pencolor()
-    # turtle.ht()
-
-    #now you can do stuff \/
-
+    lPaddle_setup()
+    rPaddle_setup()
 main()
-    
